b964ad0a2c5dcac0e1081cc6270a35a8e63500f7
vinayak s kaimal <vinayakskaimal372001@gmail.com>
2024-10-05 15:12:40 +0530

    mess

---
+++ action.js
@@ -0,0 +1,60 @@
+// Select the class bubble
+time = document.getElementsByClassName('bubbles')[0];
+
+// padding values for desktop
+var fish2move = 100;
+var fish3move = 900;
+var fish4move = 1200;
+
+if (screen.width < 400) {
+
+    //Change transformation duration and translatey for mobile view
+    time.style.setProperty('--transform-duration', '15s')
+    time.style.setProperty('--transform-y', '-700vh')
+
+    // padding values for mobile
+    fish2move = 1680;
+    fish3move = 3000;
+    fish4move = 4300;
+}
+
+
+
+window.addEventListener('scroll', function () {
+
+    let value = window.scrollY;   //Get Scroll Value (Mobile - High)
+
+    text.style.top = 50 + value * -0.2 + '%';
+    cloud.style.left = value * 2 + 'px';
+
+    bird1.style.top = value * 0.1 + 'px';
+    bird1.style.left = value * 1 + 'px';
+
+    bird2.style.top = value * -0.1 + 'px';
+    bird2.style.left = value * -2 + 'px';
+
+    explore.style.marginTop = value * 1.5 + 'px';
+
+    rocks.style.top = value * -0.14 + 'px';
+
+    forest.style.top = value * 0.4 + 'px';
+    sky.style.top = value * 0.25 + 'px';
+    mountains.style.top = value * 0.25 + 'px';
+
+    header.style.top = value * 0.7 + 'px';
+    sun.style.top = value * 1 + 'px';
+
+    //To prevent splash to move above sea water
+    if (value < 380) {
+        splash.style.top = 20 + value * -0.3 + 'px';
+    }
+
+    //Move fishes horizontally
+    fish1.style.right = (value - 100) * 1 + 'px';
+    fish2.style.left = (value - fish2move) * 1 + 'px';
+    fish3.style.right = (value - fish3move) * 1 + 'px';
+    fish4.style.left = (value - fish4move) * 1 + 'px';
+})
+
+
+//
